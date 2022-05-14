2a85c317433fa324418b70afac3b37953a229e2a
Farshad Barahimi <farshad.barahimi@dal.ca>
2022-05-14 09:55:21 -0300

    better

--- data_sets_preparation_source_code/genomes_distances/genomes_distances_1.go
+++ data_sets_preparation_source_code/genomes_distances/genomes_distances_1.go
@@ -12,8 +12,12 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/farshad-barahimi-academic-codes/data_sets_preparation/helpers"
+	"io/ioutil"
+	"math"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -28,7 +32,7 @@ func (genomesDistancesPreparation1 GenomesDistancesPreparation1) Prepare(dataSet
 
 	var debug bool = false
 
-	cmd := exec.Command(plinkPath, "--pfile", "genomes", "--make-rel", "--memory", "30000")
+	cmd := exec.Command(plinkPath, "--pfile", "genomes", "--make-rel", "square", "--out", "matrix", "--memory", "30000")
 
 	cmd.Dir = filepath.Join(outputDirectory, "Temporary_files")
 
@@ -69,6 +73,84 @@ func (genomesDistancesPreparation1 GenomesDistancesPreparation1) Prepare(dataSet
 
 	fmt.Println("Running PLINK 2 finished.")
 	fmt.Println(time.Now().Format(time.UnixDate))
+
+	labels := make(map[string]int)
+	labels["EAS"] = 1
+	labels["EUR"] = 2
+	labels["AFR"] = 3
+	labels["AMR"] = 4
+	labels["SAS"] = 5
+
+	labelsFileContent, err := ioutil.ReadFile(filepath.Join(outputDirectory, "Downloaded_files", "integrated_call_samples_v3.20130502.ALL.panel"))
+	if err != nil {
+		panic("Not finished successfully.")
+	}
+	labelsFileLines := strings.Split(string(labelsFileContent), "\n")[1:]
+
+	labelNumbers := make(map[string]int)
+
+	for _, labelsFileLine := range labelsFileLines {
+		line := strings.TrimSpace(labelsFileLine)
+		if len(line) == 0 {
+			continue
+		}
+
+		labelNumbers[strings.Split(line, "\t")[0]] = labels[strings.Split(line, "\t")[2]] - 1
+	}
+
+	matrixIDsFileContent, err := ioutil.ReadFile(filepath.Join(outputDirectory, "Temporary_files", "matrix.rel.id"))
+	if err != nil {
+		panic("Not finished successfully.")
+	}
+	matrixIDsLines := strings.Split(string(matrixIDsFileContent), "\n")[1:]
+
+	matrixIDs := make([]string, 0, len(matrixIDsLines))
+
+	for _, matrixIDLine := range matrixIDsLines {
+		matrixID := strings.TrimSpace(matrixIDLine)
+		if len(matrixID) != 0 {
+			matrixIDs = append(matrixIDs, matrixID)
+		}
+	}
+
+	matrixFile := filepath.Join(outputDirectory, "Temporary_files", "matrix.rel")
+	matrixFileContent, err := ioutil.ReadFile(matrixFile)
+	if err != nil {
+		panic("Not finished successfully.")
+	}
+	matrixLines := strings.Split(string(matrixFileContent), "\n")
+	maximumFloat := -math.MaxFloat64
+	floatNumbers := make([][]float64, 0, len(matrixLines))
+	for i := 0; i < len(matrixLines); i++ {
+		matrixLine := matrixLines[i]
+		if len(strings.TrimSpace(matrixLine)) == 0 {
+			continue
+		}
+		floatNumbers = append(floatNumbers, make([]float64, 0, len(matrixLines)))
+		numbers := strings.Split(matrixLine, "\t")
+		for _, number := range numbers {
+			if len(strings.TrimSpace(number)) == 0 {
+				continue
+			}
+
+			floatNumber, _ := strconv.ParseFloat(strings.TrimSpace(number), 64)
+			maximumFloat = math.Max(maximumFloat, floatNumber)
+			floatNumbers[i] = append(floatNumbers[i], floatNumber)
+		}
+	}
+
+	distancesFile := filepath.Join(outputDirectory, "Final_files", "distances.csv")
+	distancesCSV := strings.Builder{}
+
+	for i := 0; i < len(floatNumbers); i++ {
+		distancesCSV.WriteString(strconv.Itoa(labelNumbers[matrixIDs[i]]))
+		for j := 0; j < len(floatNumbers); j++ {
+			distancesCSV.WriteString(",")
+			distancesCSV.WriteString(strconv.FormatFloat(maximumFloat-floatNumbers[i][j], 'f', 10, 64))
+		}
+		distancesCSV.WriteString("\r\n")
+	}
+	ioutil.WriteFile(distancesFile, []byte(distancesCSV.String()), 600)
 }
 
 func PrepareGenomeDistances1(outputDirectory string) {
